d10a16a6cbf66780a845d15405a49fb5402ea4eb
alejandro <luis.vejarano01@correo.usa.edu.co>
2020-09-30 17:06:29 -0500

    Agrego gráfico de barras por edad

--- main.py
+++ main.py
@@ -24,9 +24,11 @@ results = client.get("gt2j-8ykr",query="SELECT ciudad_de_ubicaci_n as ciudad, co
 # Convert to pandas DataFrame
 df = pd.DataFrame.from_records(results)
 se = pd.DataFrame.from_records(client.get("gt2j-8ykr", query="SELECT sexo, count(sexo) as ctdGenero GROUP BY sexo ORDER BY sexo"))
+ed= pd.DataFrame.from_records(client.get("gt2j-8ykr", query="SELECT edad, count(edad) as cont GROUP BY edad ORDER BY edad"))
 #print(se.columns)
-
+print(ed)
 #print(df.dtypes)
+
 ciudad=[]
 cantidad=[]
 valGenero=[0,0]
@@ -59,7 +61,7 @@ ax.invert_yaxis()
 ax.set_xlabel('Número de Personas Contagiadas')
 ax.set_title('Contagios Covid-19 Colombia')
 
-
+#Diagrama de Torta por Ciudades
 labels = ciudad
 sizes = cantidad
   # only "explode" the 2nd slice (i.e. 'Hogs')
@@ -68,16 +70,45 @@ fig1, ax1 = pt.subplots(figsize=(10,7))
 ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+###
+
+#Diagrama de Barras por edad y Género
+
+for x in range(len(se['sexo'])):
+    if(se['sexo'][x] == 'M' or se['sexo'][x] == 'm'):
+        valGenero[0]+= int(se['ctdGenero'][x])
+    else:
+        valGenero[1]+= int(se['ctdGenero'][x])
+
+agecant=[0,0,0,0,0]
+
+age=['NIÑOS\n0-12\naños','ADOLESCENTES\n13-18\naños','JOVENES\n19-26\naños','ADULTOS\n26-59\naños','ANCIANOS\n60+\naños']
+
+for x in range(len(ed['edad'])):
+    aux= int(ed['edad'][x])
+    aux2= int(ed['cont'][x])
+    if aux<12:
+        agecant[0]+=aux2
+    elif (aux>=12 and aux<=18):
+        agecant[1]+=aux2
+    elif (aux>18 and aux<=26):
+        agecant[2]+=aux2
+    elif (aux>26 and aux<=59):
+        agecant[3] += aux2
+    else :
+        agecant[4]+=aux2
+
+
+
+fig, ax = pt.subplots()
+ax.set_ylabel('Número de Casos')
+ax.set_title('Casos Confirmados Por Edades')
+pt.bar(age, agecant)
+pt.savefig('gr.png')
 
 
 
 
 
 
-
-
-
-
-
-pt.grid() #Activa la cuadricula
 pt.show() #Muestra la gráfica
